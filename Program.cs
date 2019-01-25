dc1a6880dba285e44ff0e77295fc4b3870ba6693
Dirk Dobson <dirkvancouver@live.com>
2019-01-24 18:54:19 -0700

    contructor ;

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 
 namespace constructors
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var customer = new Customer(1, "John");
+            System.Console.WriteLine(customer.Id);
+            System.Console.WriteLine(customer.Name);
+            
         }
     }
 }
