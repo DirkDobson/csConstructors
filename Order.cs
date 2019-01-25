c89f0fa9c161afab3faf689798049aed2d8175f9
Dirk Dobson <dirkvancouver@live.com>
2019-01-24 19:24:05 -0700

    : this ()

---
+++ Order.cs
@@ -0,0 +1,10 @@
+  using System;
+  
+  namespace constructors
+  {
+  public class Order
+    {
+        
+    }
+
+  }
--- Program.cs
+++ Program.cs
@@ -2,12 +2,17 @@
 
 namespace constructors
 {
-    
+  
     class Program
     {
         static void Main(string[] args)
         {
             var customer = new Customer(1, "John");
+
+            var order = new Order();
+
+            customer.Orders.Add(order);
+
             System.Console.WriteLine(customer.Id);
             System.Console.WriteLine(customer.Name);
             
